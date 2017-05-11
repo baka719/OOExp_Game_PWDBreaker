2ad8a8c430569b7181304a68bf8d22aace2f9327
Chiikyu <baka719@qq.com>
2017-05-11 17:52:25 +0800

    PWD Breaker_1st uploading
    Maybe will be the last updating..

---
+++ Chiikyu/Chiikyu/Chiikyu/src/mainClass.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class mainClass {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		Windows win = new Windows();
+
+	}
+
+}
